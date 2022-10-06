8abaae54cab91054a566a2bba74105b4b0c704ce
lichuangbo <15503609389@163.com>
2022-10-06 11:23:55 +0800

    回调

---
+++ src/main/java/cn/edu/tit/template/AClass.java
@@ -0,0 +1,13 @@
+package cn.edu.tit.template;
+
+public class AClass {
+  public static void main(String[] args) {
+    BClass b = new BClass();
+    b.process(new ICallback() { //回调对象
+      @Override
+      public void methodToCallback() {
+        System.out.println("Call back me.");
+      }
+    });
+  }
+}
---
+++ src/main/java/cn/edu/tit/template/BClass.java
@@ -0,0 +1,8 @@
+package cn.edu.tit.template;
+
+public class BClass {
+  public void process(ICallback callback) {
+    //...
+    callback.methodToCallback(); //... 10
+  }
+}
